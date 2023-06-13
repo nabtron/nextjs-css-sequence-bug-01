5b69787d662f205bcacea5649cf7520e687c6890
nabtron <khan@nabtron.com>
2023-06-13 11:46:41 +0500

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import './green.css';
 import './yellow.css';
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
