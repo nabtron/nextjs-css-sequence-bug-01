7401ebf3fa80d0c871d3ad6af1d23d5c92877e37
nabtron <khan@nabtron.com>
2023-06-19 17:42:49 +0500

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,8 +14,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      {/* <body>{children}</body> */}
+      {/* <body className={inter.className}>{children}</body> */}
+      <body>{children}</body>
     </html>
   ) 
 }
