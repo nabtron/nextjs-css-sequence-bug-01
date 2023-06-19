e0aae0b7b6a0cce1ec6bb214d07c1520f57064aa
nabtron <khan@nabtron.com>
2023-06-19 17:37:25 +0500

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import './globals.css';
 import './green.css';
 import './yellow.css'; 
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body>{children}</body>
     </html>
   ) 
 }
